71add306f4e40cd079c017896f8fe63ee4437ccb
nbblks <nbblks@gmail.com>
2022-03-16 18:18:55 +0900

    add infinite scroll

--- pages/posts.js
+++ pages/posts.js
@@ -2,7 +2,8 @@ import Posts from "../components/posts/posts";
 import Layout from "../components/layout";
 
 import { getSortedPostsData } from "../lib/posts";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import styles from "../styles/posts.module.css";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -14,10 +15,44 @@ export async function getStaticProps() {
 }
 
 export default function posts({ allPostsData }) {
+  const POST_COUNT = 25;
+  const [posts, setPosts] = useState(allPostsData.slice(0, POST_COUNT));
+
+  const getMorePosts = () => {
+    const startIndex = posts.length;
+    const lastIndex =
+      posts.length + POST_COUNT > allPostsData.length
+        ? allPostsData.length
+        : posts.length + POST_COUNT;
+    const toBeAdded = allPostsData.slice(startIndex, lastIndex);
+    setPosts([...posts, ...toBeAdded]);
+    setLoading(false);
+  };
+
+  const handleScroll = () => {
+    const lastLoaded = document.querySelector("ul > li:last-child");
+    if (lastLoaded) {
+      const lastLoadedOffset = lastLoaded.offsetTop + lastLoaded.clientHeight;
+      const pageOffset = window.scrollY + window.innerHeight;
+
+      if (pageOffset > lastLoadedOffset) {
+        getMorePosts();
+      }
+    }
+  };
+
+  // listen to scroll positions for loading more posts on scroll
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
     <Layout home>
-      <div className={styles.home}>
-        <Posts posts={allPostsData} />
+      <div className={styles.posts}>
+        <Posts posts={posts} />
       </div>
     </Layout>
   );
