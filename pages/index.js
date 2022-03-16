77f45034614284537c69b3600eb8eb13b176434b
nbblks <nbblks@gmail.com>
2022-03-16 20:55:05 +0900

    change landing page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
-import Posts from '../components/posts/Posts';
-import { getSortedPostsData } from '../lib/posts';
-import Layout from '../components/Layout';
+import About from "./about";
 
-import styles from '../styles/Home.module.css';
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData
-    }
-  };
-};
-
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <div className={styles.home}>
-        <Posts posts={allPostsData}/>
-      </div>
-    </Layout>
-  );
-};
+export default function Home() {
+  return <About />;
+}
